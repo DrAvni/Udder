0d7c7696d10096ea7e9d948926d6dd3daa5f648b
Avni Rexhaj <AvniRexhaj@hotmail.com>
2020-05-10 00:41:47 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Avni Rexhaj on 5/10/20.
+//  Copyright © 2020 Avni Rexhaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
